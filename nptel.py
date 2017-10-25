aa010191008e80163769ff57114ca04352812f17
vidulkumar <30744970+vidulkumar@users.noreply.github.com>
2017-10-25 17:24:45 +0530

    updated nptel

--- nptel.py
+++ nptel.py
@@ -1,7 +1,16 @@
-def g(n):
-  for i in range(2,n):
-    if n%i == 0:
+'''
+Objective: this code checks if a number is prime or not
+return Value: True for Prime
+              False for non-Prime
+Changes done: range is decreased from 2-num to 2-num//2
+              it will work more efficiently as compared to last code
+'''
+
+
+def checkPrime(num):
+  for i in range(2,num//2):
+    if num%i == 0:
        return(False)
 
   return(True)
-print(g(5))
+print(checkPrime(5))
